e559f578abfc3ed2ef27c22866a947d303c94c9d
Jonathan Sanderson <lists@quernstone.com>
2019-04-22 23:31:06 +0100

    Text console output

--- databot.py
+++ databot.py
@@ -2,6 +2,8 @@
 """
 
 import pyowm
+import time
+from threading import Timer
 from pypollen import Pollen
 from clientsecrets import owmkey
 
@@ -10,14 +12,98 @@ owm = pyowm.OWM(owmkey)
 latitude = 55.03973
 longitude = -1.44713
 
-# OpenWeatherMap current report
-# observation = owm.weather_at_place('Whitley Bay,GB')
-observation = owm.weather_at_id(2634032) # Prefer this approach if possible
-w = observation.get_weather()
-print(w)
+interval = 10 # seconds between API data updates
 
-pressure = w.get_pressure()['press']
-print(pressure)
+pressureReport = 0
+pollenReport = "NAN"
 
-# Benadryl Pollen Forecast data
-print(Pollen(latitude, longitude).pollencount)
+class RepeatedTimer(object):
+    """Simple timer class, from StackExchange (obviously).
+
+    Credit: user MestreLion, https://stackoverflow.com/questions/3393612
+
+    Modified to allow change of interval without tearing down the timer.
+    """
+
+    def __init__(self, interval, function, *args):
+        """Initialize the timer object."""
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        """Timer has been triggered: execute callback function."""
+        self.is_running = False
+        self.start()
+        self.function(*self.args)
+
+    def start(self):
+        """Start the timer, if it's not already running."""
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        """Stop the timer."""
+        self._timer.cancel()
+        self.is_running = False
+
+    def bpm(self, newBPM):
+        """Reset the interval to new beats per minute."""
+        self.interval = (60/newBPM)
+
+
+def updatePressure():
+    """Fetch pressure data from OpenWeatherMap.
+    
+    Triggered on a timer to avoid overloading API.
+    """
+    global pressureReport
+    # observation = owm.weather_at_place('Whitley Bay,GB')
+    observation = owm.weather_at_id(2634032) # Prefer this approach if possible
+    w = observation.get_weather()
+    pressureReport = w.get_pressure()['press']
+    return pressureReport
+
+
+def updatePollen():
+    """Fetch pollen data from Benadryle / Met. Office.
+
+    Triggered on a timer to avoid overloading API.
+    """
+    global pollenReport
+    pollenReport = Pollen(latitude, longitude).pollencount
+    return pollenReport
+
+
+def displayClock():
+    """Documentation string.
+    """
+
+    timeString = time.strftime("%H:%M")
+    print(timeString)
+    time.sleep(1)
+
+
+if __name__ == "__main__":
+    
+    # Get data on initial start
+    pressureReport = updatePressure()
+    pollenReport = updatePollen()
+
+    # Start the data update timers
+    rtPressure = RepeatedTimer(interval, updatePressure)
+    rtPollen = RepeatedTimer(interval, updatePollen)
+
+    # ...and now we can loop
+    while True:
+        displayClock()
+        displayClock()
+        print(pressureReport)
+        time.sleep(2)
+        print(pollenReport)
+        time.sleep(2)
